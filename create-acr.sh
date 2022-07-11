0717a31a3622e606022208b7096d8fea77dafa0c
bashacse <myfriendbasha@gmail.com>
2022-07-11 19:21:07 +0530

    added delete resource command in create-acr script

--- create-acr.sh
+++ create-acr.sh
@@ -11,3 +11,7 @@ az group create --name $ACR_RESOURCE_GROUP_NAME --location $LOCATION
  
 # Create a container registry that will be where you deploy your image to
 az acr create --resource-group $ACR_RESOURCE_GROUP_NAME --name $ACR_NAME --sku Basic
+
+
+##Delete the resource
+##az group delete --name $ACR_RESOURCE_GROUP_NAME
